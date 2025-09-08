a6e579cc6c72cf78926bcfd5629ee6a46ced6a03
alfiy <alfiy@163.com>
2025-09-08 13:33:20 +0800

    edit css main.js passowrd-confirm.js

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,8 @@ import { init as initUserManagement } from './userManagement.js';
 // 从 installUninstall.js 模块中导入统一的初始化函数
 import { init as initInstallUninstall } from './installUninstall.js';
 // 可以根据需要，为其他模块创建并导入 init 函数
-import {init as PasswordConfirm } from './password-confirm.js';
+// import {init as PasswordConfirm } from './password-confirm.js';
+import { init as ChangePassword } from './changePassword.js';
 
 /**
  * 统一绑定所有模块的事件和初始化逻辑
@@ -32,7 +33,8 @@ function bindAll() {
     }
 
     // 可以在这里绑定全局通用的事件，例如导航栏点击事件等
-    PasswordConfirm();
+    // PasswordConfirm();
+    ChangePassword();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
